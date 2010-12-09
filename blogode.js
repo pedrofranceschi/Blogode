b987c07644375991898acacc9ee6db19c7ea0f84
pH <pedrohfranceschi@gmail.com>
2010-12-09 00:56:59 -0200

    Implemented posts creation, delete, update and read in the admin page.

--- blogode.js
+++ blogode.js
@@ -16,12 +16,12 @@ app.configure(function() {
 });
 
 var posts = require('./lib/posts');
-var admin = require('./lib/admin');
+var users = require('./lib/users');
 
 app.get("/", function(req, res){
     // return posts list
     
-    posts.getPosts(function (posts){
+    posts.getPosts(10, function (posts){
         res.render('index', {
             locals: { 'posts': posts }
         });
@@ -31,11 +31,11 @@ app.get("/", function(req, res){
 app.get("/admin", function(req, res){
     // return admin panel
     
-    if(!req.session.admin_username) {
+    if(!req.session.username) {
         return res.redirect("/admin/login");   
     }
     
-    res.render('admin_panel', {
+    res.render('admin/panel', {
         layout: false
     });
 });
@@ -44,11 +44,11 @@ app.get("/admin", function(req, res){
 app.get("/admin/login", function(req, res){
     // return admin login page
     
-    if(req.session.admin_username) {
+    if(req.session.username) {
         return res.redirect("/admin")
     }
     
-    res.render('admin_login', {
+    res.render('admin/login', {
         layout: false
     });
 });
@@ -60,11 +60,87 @@ app.post("/admin/authenticate", function(req, res){
         res.redirect('/admin/login')
     }
     
-    admin.verifyCredentials(req.param('username'), req.param('password'), function(isAdmin){
-        if(isAdmin) {
-            req.session.admin_username = req.param('username')
+    users.verifyCredentials(req.param('username'), req.param('password'), function(isValidUser, userId){
+        if(isValidUser) {
+            req.session.username = req.param('username');
+            req.session.user_id = userId;
         }
         res.redirect("/admin");
+    });
+});
+
+app.get('/admin/posts', function(req, res) {
+    // return the list of posts (as admin)
+    
+    if(!req.session.username)  {
+        return res.redirect("/admin/login")
+    }
+    posts.getPosts(0, function (posts){
+        res.render('admin/posts/index', {
+            layout: false,
+            locals: { 'posts': posts }
+        });
+    });
+});
+
+app.get('/admin/posts/new', function(req, res) {
+    // return the formulary to create a new post
+    
+    if(!req.session.username)  {
+        return res.redirect("/admin/login")
+    }
+    res.render('admin/posts/new', {
+        layout: false
+    });
+});
+
+app.get('/admin/posts/:id', function(req, res) {
+    // return a post (to edit)
+    
+    if(!req.session.username)  {
+        return res.redirect("/admin/login")
+    }
+    posts.getPost(req.param('id'), function (post){
+        res.render('admin/posts/edit', {
+            layout: false,
+            locals: { 'post': post }
+        });
+    });
+});
+
+app.post('/admin/posts/save', function(req, res) {
+    if(!req.session.username)  {
+        return res.redirect("/admin/login")
+    }
+    if(!req.param('title') || !req.param('body')) {
+        return res.redirect("/admin/posts/new");
+    }
+    posts.createPost(req.param('title'), req.param('body'), req.session.user_id, function(postId) {
+        return res.redirect('/admin/posts/' + postId);
+    });
+});
+
+app.post('/admin/posts/update', function(req, res) {
+    if(!req.session.username)  {
+        return res.redirect("/admin/login")
+    }
+    if(!req.param('title') || !req.param('body')) {
+        return res.redirect("/admin/posts/new");
+    }
+    posts.updatePost(req.param('id'), req.param('title'), req.param('body'), function() {
+        return res.redirect('/admin/posts/' + req.param('id'));
+    });
+});
+
+app.get('/admin/posts/destroy/:id', function(req, res) {
+    if(!req.session.username)  {
+        return res.redirect("/admin/login")
+    }
+    if(!req.param('id')) {
+        return res.redirect("/admin/posts/");
+    }
+    posts.destroyPost(req.param('id'), function () {
+        return res.redirect('/admin/posts/')
     });
 });
 
